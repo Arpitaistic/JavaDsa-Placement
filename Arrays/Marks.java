c977bb048c5207329b244325a60447336c5f277a
Arpita <arpitaistic@gmail.com>
2025-08-15 22:16:03 +0530

    added Marks in Arrays

--- Arrays/Marks.java
+++ Arrays/Marks.java
@@ -4,6 +4,7 @@ public class Marks {
     public static void main(String[] args) {
         // Declare and initialize an array of marks
         int[] marks = new int [5];
+        int [] marks1 = {45, 26, 31};
         marks[0] = 97;
         marks[1] = 95;
         marks[2] = 56;
